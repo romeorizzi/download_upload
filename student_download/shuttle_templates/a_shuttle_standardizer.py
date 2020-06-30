98a1f35992d99533d2e2fb5c07703e4a9f0d19aa
Romeo Rizzi <romeo.rizzi@univr.it>
2020-06-30 06:15:55 +0200

    aggiunto script a_shuttle_standardizer.py e sistemato problema causato da ettere accentate nei nomi nel messaggio di benvenuto

--- student_download/shuttle_templates/a_shuttle_standardizer.py
+++ student_download/shuttle_templates/a_shuttle_standardizer.py
@@ -5,20 +5,20 @@ from shutil import copyfile
 import csv
 import re
 
-CSV_FILE_WITH_STUDENTS_GROUP = "../lista_studenti_iscritti_con_chiavi.csv"
+CSV_FILE_WITH_STUDENTS = "../lista_studenti_iscritti_con_chiavi.csv"
 SHUTTLE_FOLDER = "../shuttle"
 SHUTTLE_TEMPLATES_FOLDER = "../shuttle_templates"
 GEN_EXAM_VERSION = "../shuttle_templates/template_testo_esame_dummy"
 
 
-# script che, per ogni studente che appaia nel file CSV_FILE_WITH_STUDENTS_GROUP genera un testo di tema dummy dal template e lo pone in shuttle.
+# script che ricostruisce correttamente i file compressi da un formato sballato. Conviene tenerlo perchè potrebbe venire comodo anche in futuro nello ristandardizzare i subfolders nello shuttle.
 
 def usage(onstream):
-    print("\nSono lo script che genera tanti temi nominativi quanti sono gli studenti ma per semplice copia da un'unico template di tema.\n\nUsage: os.path.basename(argv[0])  yyyy-mm-dd  [ GEN_EXAM_VERSION [ SHUTTLE_FOLDER [ CSV_FILE_WITH_STUDENTS_GROUP ] ] ]\n\n   The first parameter is the date of the exam.\n   I tre parametri opzionali che vanno semmai precisati nell'ordine sono:\n   GEN_EXAM_VERSION (default {GEN_EXAM_VERSION}\n   SHUTTLE_FOLDER (default {SHUTTLE_FOLDER}\n   CSV_FILE_WITH_STUDENTS_GROUP (default {CSV_FILE_WITH_STUDENTS_GROUP})", file=onstream)
+    print("\nSono uno script che ristandardizza i nomi e la strutturazione dei file presenti nei subfolders dello shuttle.\n\nUsage: os.path.basename(argv[0])  yyyy-mm-dd  [ SHUTTLE_FOLDER [ CSV_FILE_WITH_STUDENTS ] ] ]\n\n   The first parameter is the date of the exam.\n   I due parametri opzionali che vanno semmai precisati nell'ordine sono:\n   SHUTTLE_FOLDER (default {SHUTTLE_FOLDER}\n   CSV_FILE_WITH_STUDENTS (default {CSV_FILE_WITH_STUDENTS})", file=onstream)
 
 # THE MAIN PROGRAM:
-if len(argv) > 5:
-    print("ERROR: Hai fornito troppi parametri (più di 4)!", file=stderr)
+if len(argv) > 4:
+    print("ERROR: Hai fornito troppi parametri (più di 3)!", file=stderr)
     usage()
     exit(1)
     
@@ -38,18 +38,14 @@ if len(argv) > 2:
 if len(argv) > 3:
     SHUTTLE_FOLDER = argv[3]
 if len(argv) > 4:
-    CSV_FILE_WITH_STUDENTS_GROUP = argv[4]
+    CSV_FILE_WITH_STUDENTS = argv[4]
 
-if not os.path.exists(GEN_EXAM_VERSION):
-    print(f"Questo script ({argv[0]}) opera a partire dalla cartella GEN_EXAM_VERSION che contiene il testo del tema unico da distribuire a tutto il gruppo di studenti listati nel file CSV_FILE_WITH_STUDENTS_GROUP={CSV_FILE_WITH_STUDENTS_GROUP}.\n\nESECUZIONE INTERROTTA: File GEN_EXAM_VERSION={GEN_EXAM_VERSION} non trovato.", file=stderr)
-    exit(1)
-    
 if not os.path.exists(SHUTTLE_FOLDER):
-    print(f"Questo script ({argv[0]}) ripone nel folder SHUTTLE_FOLDER il testo del tema di esame che si trova nella cartella GEN_EXAM_VERSION.\n\nESECUZIONE INTERROTTA: File SHUTTLE_FOLDER={SHUTTLE_FOLDER} non trovato.", file=stderr)
+    print(f"Questo script ({argv[0]}) ristruttura i subfolder presenti nel folder SHUTTLE_FOLDER.\n\nESECUZIONE INTERROTTA: File SHUTTLE_FOLDER={SHUTTLE_FOLDER} non trovato.", file=stderr)
     exit(1)
     
-if not os.path.exists(CSV_FILE_WITH_STUDENTS_GROUP):
-    print(f"Questo script ({argv[0]}) opera a partire dal file CSV_FILE_WITH_STUDENTS_GROUP che contiene i dati degli studenti necessari all'invio delle mail che forniscano loro i punti di accesso e le credenziali personali.\n\nESECUZIONE INTERROTTA: File CSV_FILE_WITH_STUDENTS_GROUP={CSV_FILE_WITH_STUDENTS_GROUP} non trovato.", file=stderr)
+if not os.path.exists(CSV_FILE_WITH_STUDENTS):
+    print(f"Questo script ({argv[0]}) fà riferimento al file CSV_FILE_WITH_STUDENTS che contiene i dati degli studenti necessari all'invio delle mail che forniscano loro i punti di accesso e le credenziali personali.\n\nESECUZIONE INTERROTTA: File CSV_FILE_WITH_STUDENTS={CSV_FILE_WITH_STUDENTS} non trovato.", file=stderr)
     exit(1)
 
 def clean_name(first_name_of_a_person):
@@ -58,7 +54,7 @@ def clean_name(first_name_of_a_person):
     
 risp = os.system(f"cd {SHUTTLE_FOLDER}") # per prima cosa ci portiamo dentro lo shuttle in allestimento
 risp = os.system(f"cp {SHUTTLE_TEMPLATES_FOLDER}/.htaccess {SHUTTLE_FOLDER}/") # quindi si oscura il listing della cartelle per il dopo atterraggio dello shuttle
-with open(f"{CSV_FILE_WITH_STUDENTS_GROUP}") as input_file:
+with open(f"{CSV_FILE_WITH_STUDENTS}") as input_file:
     for row in list(csv.reader(input_file)):
         DEST_STUDENT_CODE = row[1]
         DEST_ANCHOR = row[3]
@@ -68,8 +64,12 @@ with open(f"{CSV_FILE_WITH_STUDENTS_GROUP}") as input_file:
         DEST_NAME = row[6]
         DEST_SURNAME = row[7]
 
-        print(f"genero la cartella del testo d'esame per lo studente {DEST_STUDENT_CODE} {DEST_ID} {DEST_NAME} {DEST_SURNAME}:")
-        risp = os.system(f"cp -r {GEN_EXAM_VERSION} esame_RO-{DATE}_{DEST_ID}")
+        print(f"rigenero la cartella del testo d'esame per lo studente {DEST_STUDENT_CODE} {DEST_ID} {DEST_NAME} {DEST_SURNAME}:")
+        risp = os.system(f"mkdir esame_RO-{DATE}_{DEST_ID}")
+        risp = os.system(f"mv esameRO_{DATE}_{DEST_ANCHOR}_{DEST_ID}/esameRO-{DATE}_{DEST_ANCHOR}_{DEST_ID}.zip esame_RO-{DATE}_{DEST_ID}/")
+        risp = os.system(f"unzip esame_RO-{DATE}_{DEST_ID}/esameRO-{DATE}_{DEST_ANCHOR}_{DEST_ID}.zip")
+        risp = os.system(f"rm -f esame_RO-{DATE}_{DEST_ID}/esameRO-{DATE}_{DEST_ANCHOR}_{DEST_ID}.zip")
+        risp = os.system(f"rm -rf esameRO_{DATE}_{DEST_ANCHOR}_{DEST_ID}")
         risp = os.system(f"echo Benvenuto {clean_name(DEST_NAME)} nel folder dove elaborerai il tuo esame e che infine, dopo aver trasformato in un file compresso, ci consegnerai alla fine. > esame_RO-{DATE}_{DEST_ID}/BENVENUTO.md")
         risp = os.system(f"tar -cvzf esame_RO-{DATE}_{DEST_ID}.tgz esame_RO-{DATE}_{DEST_ID}")
         risp = os.system(f"zip -r esame_RO-{DATE}_{DEST_ID} esame_RO-{DATE}_{DEST_ID}")
