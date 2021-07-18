9faf47f7f72dac50365733dade656b01199e712b
Romeo Rizzi <romeo.rizzi@univr.it>
2021-07-18 02:13:16 +0200

    pulita la parte per il cloning su web server

--- student_download/VPN_clone_shuttle/clone_shuttle.sh
+++ student_download/VPN_clone_shuttle/clone_shuttle.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-echo "per entrare su profs devi prima attivare la VPN. Lei ti bloccherà il browser."
-echo "l'accesso a profs NON mi richiede attualmente le credenziali in quanto profs ha la mia chiave prubblica."
+echo "Per entrare su profs devi prima attivare la VPN (credenziali GIA, e meccanismo di Dario così non si bloccherà il browser ed altre connessioni correntemente aperte."
+echo "Attivata la VPN, l'accesso a profs NON mi richiede attualmente le credenziali in quanto profs ha la mia chiave pubblica."
+start_time=`date +%s`
 scp -r ../shuttle rzzrmo30@profs.sci.univr.it:~/public_html/classes/RO/
+current_time=`date +%s`
+total_time=$((current_time-start_time))
+echo 
+echo "Clonare lo shuttle sul web server ha preso TEMPO TOTALE= $total_time"
+exit 0
--- student_download/VPN_clone_shuttle/incr_add_to_clone_shuttle.sh
+++ student_download/VPN_clone_shuttle/incr_add_to_clone_shuttle.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-echo "per entrare su profs devi prima attivare la VPN. Lei ti bloccherà il browser."
-echo "l'accesso a profs NON mi richiede attualmente le credenziali in quanto profs ha la mia chiave prubblica."
+echo "Per entrare su profs devi prima attivare la VPN (credenziali GIA, e meccanismo di Dario così non si bloccherà il browser ed altre connessioni correntemente aperte."
+echo "Attivata la VPN, l'accesso a profs NON mi richiede attualmente le credenziali in quanto profs ha la mia chiave pubblica."
 scp -r ../shuttle/esame_* rzzrmo30@profs.sci.univr.it:~/public_html/classes/RO/shuttle
+current_time=`date +%s`
+total_time=$((current_time-start_time))
+echo 
+echo "Questa aggiunta incrementale allo shuttle sul web server ha preso TEMPO TOTALE= $total_time"
+exit 0
